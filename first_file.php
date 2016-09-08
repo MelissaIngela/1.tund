c60562ebad991ffbb7d6687ef24a37b38058233b
melissab <melissab@greeny.cs.tlu.ee>
2016-09-08 13:11:02 +0300

    Added my first lines of code

---
+++ first_file.php
@@ -0,0 +1,13 @@
+<?php
+
+	$firstname = "Melissa Ingela";
+	$lastname = "Bramanis";
+	
+	//echo $firstname;
+	//echo $lastname;
+	
+	// kahe stringi liitmiseks kasutan punkti
+	
+	echo $firstname." ".$lastname;
+	
+?>
